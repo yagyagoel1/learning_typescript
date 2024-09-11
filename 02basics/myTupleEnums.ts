48573b9093191c1286393fd20f57ef37caa1b78c
yagyagoel1 <yagyagoel87@gmail.com>
2024-09-11 20:19:49 +0530

    learned about tuples

---
+++ 02basics/myTupleEnums.ts
@@ -0,0 +1,14 @@
+// const user : (string|number)[]=  [1,"yagya"]
+
+let user : [string,number,boolean]
+
+user  =  ["yagya",2,true]
+
+let rgb :[number,number,number] = [244,123,113]
+
+type User = [number,string]
+
+const newUser : User =[112,"example@google.com"]
+
+newUser[1]  = "yagya.com"
+export {}
