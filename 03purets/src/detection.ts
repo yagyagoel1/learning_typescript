dc03195110d4e07b4628eb873551befa9512d962
yagyagoel1 <yagyagoel87@gmail.com>
2024-09-12 16:42:51 +0530

    leared  about in as instance of etc

---
+++ 03purets/src/detection.ts
@@ -0,0 +1,94 @@
+function detectType(val:number|string){
+    if(typeof(val)=="string")
+    {
+        return val.toLowerCase()
+    }
+    return val+3
+}
+
+
+function provideId(id:string|null){
+    if(!id){
+        console.log("please provide id")
+    return 
+    }
+    id.toLowerCase()
+}
+
+interface User{
+    name:string,
+    email:string,
+
+}
+
+interface Admin{
+    name:string,
+    email:string,
+    isAdmin:boolean
+}
+
+function isAdmin(account:User|Admin){
+    if("isAdmin" in account){
+        return account.isAdmin
+    }
+    return false;
+    
+}
+
+type Fish = {swim:()=>void}
+type Bird = {fly:()=>void}
+
+function isFish(pet: Fish|Bird):pet is Fish{
+   return (pet as Fish).swim !==undefined
+}
+
+
+function getFood(pet:Fish|Bird){
+    if(isFish(pet)){
+        pet
+        return "fish food"
+    }else{
+        pet
+        return "bird food"
+    }
+}
+
+
+interface Circle {
+    kind :"circle",
+    radius:number
+
+}
+interface Square {
+    kind:"square",
+    side:number
+}
+interface Rectangle{
+    kind:"rectangle",
+    length:number,
+    width:number
+}
+type Shape = Circle | Square|Rectangle
+
+// function getTrueShape(shape:Shape){
+
+//     if(shape.kind=="circle"){
+//         return Math.PI*shape.radius**2
+//     }
+//     return shape.side*shape.side
+// }
+
+function getArea(shape:Shape){
+    switch (shape.kind){
+        case "circle":
+            return Math.PI * shape.radius **2
+        case "square":
+            return shape.side *shape.side
+        case "rectangle":
+            return shape.length *shape.width
+        default:
+            const _defaultforshape:never = shape
+            return _defaultforshape
+    }
+    
+}
